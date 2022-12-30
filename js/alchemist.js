b220007f1c6a43fe56f654ce35f59d75756b7ebb
ruvsphysics <7883386+jhruvsphysics@users.noreply.github.com>
2022-12-30 15:36:03 -0800

    add simple animation and fix tickCount

--- js/alchemist.js
+++ js/alchemist.js
@@ -15,14 +15,12 @@ function alchemistClass() {
     this.numberOfFrames = 4;
     this.height = 48;
     this.ticksPerFrame = 5;
-    this.move = false;
+    this.alchemistMove = false;
 
     this.walkNorth = false;
     this.walkSouth = false;
     this.walkWest = false;
     this.walkEast = false;
-
-    this.prevDirection = "stationary";
     
     this.reset = function() {
         for (var eachRow = 0; eachRow < ROOM_ROWS; eachRow++) {
@@ -42,12 +40,8 @@ function alchemistClass() {
     this.move = function() {
         var nextX = this.x;
         var nextY = this.y;
-        this.tickCount++;  /*this advances the ticks per frame.  @jiho, use this for random movements
-        You will need to reset the tickCount once a direction gets set. Recommend creating a random function
-        to set 1 through 5 for states (N, S, W, E, stationary).  When state gets selected, make that true and
-        the other states false. */
 
-        this.timer = (this.timer + 1) % 90;
+        this.timer = (this.timer + 1) % (this.ticksPerFrame * 6);
 
         this.pickRandomDirection = function() {
             const DIRECTIONS = ["north", "south", "west", "east", "stationary"];
@@ -61,43 +55,25 @@ function alchemistClass() {
             switch (direction) {
                 case "north":
                     this.walkNorth = true;
-                    if (this.prevDirection != "north") {
-                        this.tickCount = 0;
-                    }
                     break;
                 case "south":
                     this.walkSouth = true;
-                    if (this.prevDirection != "south") {
-                        this.tickCount = 0;
-                    }
                     break;
                 case "west":
                     this.walkWest = true;
-                    if (this.prevDirection != "west") {
-                        this.tickCount = 0;
-                    }
                     break;
                 case "east":
                     this.walkEast = true;
-                    if (this.prevDirection != "east") {
-                        this.tickCount = 0;
-                    }
                     break;
                 case "stationary":
                     this.walkNorth = false;
                     this.walkSouth = false;
                     this.walkWest = false;
                     this.walkEast = false;
-                    if (this.prevDirection != "stationary") {
-                        this.tickCount = 0;
-                    }
                     break;
             } // end of switch
-
-            this.prevDirection = direction;
         } // end of func
 
-        // for every 3 seconds (in 30fps), pick out a random direction
         if (this.timer == 0) {
             this.pickRandomDirection();
         }
@@ -106,25 +82,31 @@ function alchemistClass() {
             nextY -= this.speed;
             direction = "north";
             this.sx = 0;
-            this.sy = 0;
+            this.sy = 0*48;
         }
         if (this.walkSouth) {
             nextY += this.speed;
             direction = "south";
             this.sx = 0;
-            this.sy = 0;
+            this.sy = 1*48;
         }
         if (this.walkWest) {
             nextX -= this.speed;
             direction = "west";
             this.sx = 0;
-            this.sy = 0;
+            this.sy = 2*48;
         }
         if (this.walkEast) {
             nextX += this.speed;
             direction = "east";
             this.sx = 0;
-            this.sy = 0;
+            this.sy = 3*48;
+        }
+
+        if (this.walkNorth || this.walkSouth || this.walkWest || this.walkEast) {
+            this.alchemistMove = true;
+        } else {
+            this.alchemistMove = false;
         }
 
         var walkIntoTileIndex = getTileTypeAtPixelCoord(nextX, nextY);
@@ -197,6 +179,9 @@ function alchemistClass() {
     }
 
     this.draw = function() {
+        if (this.alchemistMove) {
+            this.tickCount++;
+        }
         if (this.tickCount > this.ticksPerFrame) {
             this.tickCount = 0;
             if (this.frameIndex < this.numberOfFrames - 1) {
