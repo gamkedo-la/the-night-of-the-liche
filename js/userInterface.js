79ec46d2f855c6153a6a8d258d8c602d4de538b6
ruvsphysics <7883386+jhruvsphysics@users.noreply.github.com>
2023-02-17 06:23:03 -0800

    made health easier to read

--- js/userInterface.js
+++ js/userInterface.js
@@ -1,12 +1,14 @@
 function displayHealth(){
+    let emptyHeartSpriteOffset = 0;
+    let fullHeartSpriteOffset = 32;
     for(i = 0; i < player.maxHealth; i++){
         let xmodifier = 32 * i;
         let xHeartPos = 700 + xmodifier;
-        canvasContext.drawImage(heartPic, 0, 0, 32, 32, xHeartPos, 0, 32, 32);
+        canvasContext.drawImage(heartPic, emptyHeartSpriteOffset, 0, 32, 32, xHeartPos, 0, 32, 32);
     };
     for(i = 0; i < player.health; i++){
         let xmodifier = 32 * i;
         let xHeartPos = 700 + xmodifier;
-        canvasContext.drawImage(heartPic, 32, 0, 32, 32, xHeartPos, 0, 32, 32);
+        canvasContext.drawImage(heartPic, fullHeartSpriteOffset, 0, 32, 32, xHeartPos, 0, 32, 32);
     };
 };
