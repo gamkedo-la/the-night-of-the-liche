21f15392496fb845e77e18750610255e2b91f249
Vince McKeown <vmckeown79@gmail.com>
2023-03-06 18:11:18 -0500

    randomized which leaf

--- js/leafEffect.js
+++ js/leafEffect.js
@@ -2,6 +2,11 @@ var leafList = [];
 var leafAmount;
 
 function pickAmountOfLeafs(){
+    this.sx;
+    this.sy;
+    this.xPos;
+    this.yPos;
+    this.markForRemoval;
     
    leafAmount = 1+Math.random()*10;
       for (var i = 0; i < leafAmount; i++) {
@@ -30,8 +35,16 @@ function addLeaf(){
 function leafClass(){
     this.initialize = function(){
         this.xPos = 1+Math.random()*600;
-        console.log("X Pos: " + this.xPos);
         this.yPos = 1+Math.random()*600;
+        this.whichLeaf = Math.floor(1+Math.random()*3-1);
+        if(this.whichLeaf == 0){
+            this.sx = 300; 
+        } else if (this.whichLeaf == 1){
+            this.sx = 350;
+        } else if (this.whichLeaf == 2){
+            this.sx = 400;
+        }
+        this.sy = 250;
         this.markForRemoval = false;
     }
 
@@ -41,7 +54,7 @@ function leafClass(){
         }
         let windSpeed = 1;
         this.xPos += windSpeed;
-        canvasContext.drawImage(worldPics[50], 300,250, 50, 50, this.xPos, this.yPos, 50, 50); 
+        canvasContext.drawImage(worldPics[50], this.sx,this.sy, 50, 50, this.xPos, this.yPos, 50, 50); 
         
     }
 }
