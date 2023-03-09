85dba8782645628dad906da2baea50abc346d2f4
Jasmine Hegman <hegpetz@gmail.com>
2023-03-09 01:33:04 -0700

    Support more than 3 max health by scooting hearts over to the left

--- js/userInterface.js
+++ js/userInterface.js
@@ -1,14 +1,16 @@
 function displayHealth(){
     let emptyHeartSpriteOffset = 0;
     let fullHeartSpriteOffset = 32;
+    let xHeartPosStart = ( player.maxHealth === 3 ) ? 700 : (700 - (player.maxHealth - player.health) * 32);
+
     for(i = 0; i < player.maxHealth; i++){
         let xmodifier = 32 * i;
-        let xHeartPos = 700 + xmodifier;
+        let xHeartPos = xHeartPosStart + xmodifier;
         canvasContext.drawImage(heartPic, emptyHeartSpriteOffset, 0, 32, 32, xHeartPos, 0, 32, 32);
     };
     for(i = 0; i < player.health; i++){
         let xmodifier = 32 * i;
-        let xHeartPos = 700 + xmodifier;
+        let xHeartPos = xHeartPosStart + xmodifier;
         canvasContext.drawImage(heartPic, fullHeartSpriteOffset, 0, 32, 32, xHeartPos, 0, 32, 32);
     };
 };
