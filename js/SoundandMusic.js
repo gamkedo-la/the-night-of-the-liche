bc0d37af7a866f50a2f63dc62d24a5e38179ff6b
Gamkedo <chris@gamkedo.com>
2023-04-10 12:47:32 -0400

    Filename capitalization fix for sound

--- js/SoundandMusic.js
+++ js/SoundandMusic.js
@@ -1,20 +1,5 @@
 var audioFormat;
 
-// Sound //
-var alchemistIntro_1 = new SoundOverlapsClass("alchemist_intro1");
-var alchemistIntro_2 = new SoundOverlapsClass("alchemist_intro2");
-var alchemistIntro_3 = new SoundOverlapsClass("alchemist_intro3");
-var sfx_door = new SoundOverlapsClass("door");
-
-// TODO: replace these with nicer mp3s - swish.mp3 is placeholder
-var sfx_sword_attack = new SoundOverlapsClass("swish");
-var sfx_arrow_attack = new SoundOverlapsClass("arrow");
-var sfx_skeleton_attack = new SoundOverlapsClass("swish");
-var sfx_lich_attack = new SoundOverlapsClass("swish");
-
-// Music //
-var backgroundMusic = new BackgroundMusicClass("backgroundMusic");
-
 function setFormat(){
 	var audio = new Audio();
 	if (audio.canPlayType("audio/mp3")){ // true in 99.8% of browsers
@@ -73,3 +58,17 @@ function BackgroundMusicClass(filenameWithPath){
 	}
 }
 
+// Sound //
+var alchemistIntro_1 = new SoundOverlapsClass("alchemist_intro1");
+var alchemistIntro_2 = new SoundOverlapsClass("alchemist_intro2");
+var alchemistIntro_3 = new SoundOverlapsClass("alchemist_intro3");
+var sfx_door = new SoundOverlapsClass("door");
+
+// TODO: replace these with nicer mp3s - swish.mp3 is placeholder
+var sfx_sword_attack = new SoundOverlapsClass("swish");
+var sfx_arrow_attack = new SoundOverlapsClass("arrow");
+var sfx_skeleton_attack = new SoundOverlapsClass("swish");
+var sfx_lich_attack = new SoundOverlapsClass("swish");
+
+// Music //
+var backgroundMusic = new BackgroundMusicClass("backgroundMusic");
